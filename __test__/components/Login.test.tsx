de41da6bbd3e21647b3bac85b43f689b7980021d
Christian Letter <christianletter@gmail.com>
2021-11-25 00:32:42 +0100

    test(login): wrote test for checking login mutation

--- __test__/components/Login.test.tsx
+++ __test__/components/Login.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Login from '../../components/authentication/Login';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
@@ -12,14 +13,36 @@ let store = createStore(reducers, initialState);
 import { MockedProvider } from '@apollo/react-testing';
 import { LOGIN_USER } from './../../graphql/apolloClient/mutations';
 
+const mocks = [
+  {
+    request: {
+      query: LOGIN_USER,
+      variables: {
+        loginInput: {
+          email: 'test@test.com',
+          password: 'password',
+        },
+      },
+    },
+    newData: jest.fn(() => ({
+      data: {
+        login: {
+          token: 'token',
+          username: 'test',
+        },
+      },
+    })),
+  },
+];
+
 const Wrapper = ({ children }: { children: React.ReactNode }) => (
-  <MockedProvider mocks={[]} addTypename={false}>
+  <MockedProvider mocks={mocks} addTypename={false}>
     <Provider store={store}>{children}</Provider>
   </MockedProvider>
 );
 
 describe('Login component', () => {
-  it('it should render correctly the form', () => {
+  it('it should render correctly the form and the button', () => {
     render(<Login />, { wrapper: Wrapper });
     const emailInput = screen.getByPlaceholderText('Email');
     const passwordInput = screen.getByPlaceholderText('Password');
@@ -27,5 +50,18 @@ describe('Login component', () => {
     expect(emailInput).toBeInTheDocument();
     expect(passwordInput).toBeInTheDocument();
     expect(button).toBeInTheDocument();
+  });
+  it('login mutation should be called when clicking the button', async () => {
+    render(<Login />, { wrapper: Wrapper });
+    const emailInput = screen.getByPlaceholderText('Email');
+    const passwordInput = screen.getByPlaceholderText('Password');
+    const button = screen.getByRole('button');
+    userEvent.type(emailInput, 'test@test.com');
+    userEvent.type(passwordInput, 'password');
+    await waitFor(() => {
+      userEvent.click(button);
+    });
+    const mutation = mocks[0].newData;
+    await expect(mutation).toHaveBeenCalledTimes(1);
   });
 });
