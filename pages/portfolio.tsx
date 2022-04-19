40316cbe784836e6d68ddfcb62e27d54eb5862e6
Christian Letter <christianletter@gmail.com>
2022-04-19 23:10:30 +0100

    feat: created button for deleting and editing holding

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,17 +1,46 @@
 import React from 'react';
 import { Flex, Box, Button, Text, Heading } from '@chakra-ui/react';
-import Holding from '../components/Holding';
 import { uuid } from 'uuidv4';
+import { EditIcon, DeleteIcon } from '@chakra-ui/icons';
 
 import { connect } from 'react-redux';
 import { IUserInfo } from './../interfaces/interfaces';
 
 function Portfolio({ username, holdings }: IUserInfo) {
   return (
-    <Flex direction="column">
-      <Heading>All your holdings</Heading>ß
+    <Flex direction="column" width="100vw" align="center">
+      {console.log(holdings)}
+      <Heading py={6}>All your holdings</Heading>
       {holdings.map((holding) => (
-        <Holding key={uuid()} holdings={holding} />
+        <Box
+          key={uuid()}
+          width="60vh"
+          boxShadow="md"
+          rounded="md"
+          align="center"
+          p={3}
+          m={2}
+          backgroundColor="blue.100"
+        >
+          <Flex direction="row" justifyContent="space-between">
+            <Flex direction="column" pl={10}>
+              <Heading as="h2" size="md">
+                {holding.quantity +
+                  ' ' +
+                  holding.cryptoId +
+                  ' - ' +
+                  holding.location}
+              </Heading>
+              <Heading as="h2" size="md">
+                {holding.apy} %
+              </Heading>
+            </Flex>
+            <Flex direction="column">
+              <DeleteIcon />
+              <EditIcon />
+            </Flex>
+          </Flex>
+        </Box>
       ))}
     </Flex>
   );
